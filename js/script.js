3f682d4210027531978b93a8cfb00bf324d556ed
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-06 17:26:48 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Aimar Fernandez
-// Created on: Feb 2025
+// Created on: May 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 function myButtonClicked() {
-  alert("Hello, World!")
+  let answer = 0
+  const inputNumber = parseInt(document.getElementById("input-number").value)
+  for (let counter = 0; counter <= inputNumber; counter = counter + 1) {
+    answer = answer + counter
+  }
+  document.getElementById("answer").innerHTML = 
+    "<p>The answer is: " + answer + "</p>"
 }
